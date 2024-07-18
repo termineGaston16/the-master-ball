5c42b8d91b6f9cf2452802b485b23073be34be2f
Gaston Termine <gastontermine16@gmail.com>
2024-07-18 07:41:48 -0300

    as

--- src/hooks/useResults.jsx
+++ src/hooks/useResults.jsx
@@ -1,18 +1,25 @@
-import { useContext } from "react"
-import { ResultsContext } from "../context/results"
+import { useContext } from "react";
+import { ResultsContext } from "../context/results";
 
 export function useResults() {
     const { results, setResults } = useContext(ResultsContext);
+
+    // Función para extraer el anteúltimo número de la URL
+    const extractNumberFromURL = (url) => {
+        const parts = url.split('/');
+        const number = parseInt(parts[parts.length - 2], 10); // Anteúltimo dato convertido a número
+        return isNaN(number) ? 0 : number; // Devuelve 0 si no es un número
+    };
 
     // FILTRAR RESULTADOS
     const filterResults = (filter) => {
         const newList = [...results.results].sort((a, b) => {
             switch (filter) {
                 case "Número Inferior":
-                    return a.url.localeCompare(b.url);
+                    return extractNumberFromURL(a.url) - extractNumberFromURL(b.url);
 
                 case "Número Superior":
-                    return b.url.localeCompare(a.url);
+                    return extractNumberFromURL(b.url) - extractNumberFromURL(a.url);
 
                 case "Alfabéticamente":
                     return a.name.localeCompare(b.name);
